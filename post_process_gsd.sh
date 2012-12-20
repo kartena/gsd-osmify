3ecfeacceb7b22a0ca21393ca6bef312ea5a18b9
perliedman <per.liedman@kartena.se>
2012-12-20 09:40:28 +0100

    Added comments, removed special cases and add indices for "kategori"/"objekt" on all tables.

--- post_process_gsd.sh
+++ post_process_gsd.sh
@@ -2,6 +2,8 @@
 
 DB=lmv
 
+# Make sure we have the correct projection settings for RT90
+# (Not currently essential since we don't render to RT90, but why not.)
 psql $DB -c <<EOF
 update SPATIAL_REF_SYS set srtext='PROJCS["RT90.RT90_2,5_gon_V",GEOGCS["Latitude/Longitude.OpenGIS.Rikets_koordinatsystem\
 _1990",DATUM["Rikets_koordinatsystem_1990",SPHEROID["Bessel 1841",6377397.155,299.152812800003],TOWGS84[414.1, 41.3, 603.\
@@ -12,6 +14,8 @@ _1990",DATUM["Rikets_koordinatsystem_1990",SPHEROID["Bessel 1841",6377397.155,29
 ere srid=2400;
 EOF
 
+# Create indices for Terrängkartan and Vägkartan.
+# These use "kkod" as the category type column.
 TABLES=`psql -q -c "select table_name from information_schema.tables where table_schema='public' order by table_name;" $D\
 B | egrep "(terrang|vagk)"`
 
@@ -20,10 +24,13 @@ for T in $TABLES; do
     psql $DB <<EOF
         create index on $T using gist (the_geom);
         create index on $T (kkod);
+        create index on $T (kategori);
         vacuum analyze $T (the_geom);
 EOF
 done
 
+# Create indices for Tätortskartan.
+# It uses "kod" as the category type column.
 TABLES=`psql -q -c "select table_name from information_schema.tables where table_schema='public' order by table_name;" $D\
 B | egrep "tatort"`
 
@@ -32,10 +39,7 @@ for T in $TABLES; do
     psql $DB <<EOF
        create index on $T using gist (the_geom);
        create index on $T (kod);
+       create index on $T (objekt);
        vacuum analyze $T (the_geom);
 EOF
 done
-
-psql $DB -c "create index on vagk_my (kategori)"
-psql $DB -c "create index on terrang_my (kategori)"
-psql $DB -c "create index on tatort_my (objekt)"
