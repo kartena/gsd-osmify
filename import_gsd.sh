7262dcd513af0391ad868d20706b6ddbd8451f9e
perliedman <per.liedman@kartena.se>
2012-12-20 09:40:00 +0100

    Broke out shp2pgsql command path to variable. Import all of Tätortskartan, not only 0336.

--- import_gsd.sh
+++ import_gsd.sh
@@ -5,6 +5,7 @@ DB=lmv
 GSD_PATH=/media/local/MapData/lmv-shape
 SQL_LOG=/dev/null
 ERROR_LOG=gsd_import.log
+SHP2PGSQL=/usr/lib/postgresql/9.1/bin/shp2pgsql
 
 # Create a clean slate
 rm $SQL_LOG
@@ -17,7 +18,7 @@ createdb -T template_postgis $DB
 echo Creating structure for Terrängkartan
 for F in $GSD_PATH/terrang/south/*.shp; do
     TABLE_NAME=terrang_`basename $F .shp | cut -d_ -f1`
-    /usr/lib/postgresql/9.1/bin/shp2pgsql -p -W latin1 -I $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
+    $SHP2PGSQL -p -W latin1 -I $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
 done
 
 # Actually import Terrängkartan, using text labels from gistext directory
@@ -25,28 +26,29 @@ FILES="`find $GSD_PATH/terrang/ -maxdepth 2 -name "*.shp" -not \( -name "tx_*.sh
 for F in $FILES; do
     TABLE_NAME=terrang_`basename $F .shp | cut -d_ -f1`
     echo Importing $F into $TABLE_NAME
-    /usr/lib/postgresql/9.1/bin/shp2pgsql -a -W latin1 $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
+    $SHP2PGSQL -a -W latin1 $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
 done
 
 # Create tables for Tätortskartan before actually inserting to them
 echo Creating structure for Tätortskartan
 for F in $GSD_PATH/tatort/0336/*.shp; do
     TABLE_NAME=tatort_`basename $F .shp | cut -d_ -f1`
-    /usr/lib/postgresql/9.1/bin/shp2pgsql -p -W latin1 -I $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
+    $SHP2PGSQL -p -W latin1 -I $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
 done
 
 # Actually import Tätortskartan
-for F in `find $GSD_PATH/tatort/0336/ -name "*.shp"`; do
+FILES="`find $GSD_PATH/tatort/ -maxdepth 2 -name "*.shp"`"
+for F in $FILES; do
     TABLE_NAME=tatort_`basename $F .shp | cut -d_ -f1`
     echo Importing $F into $TABLE_NAME
-    /usr/lib/postgresql/9.1/bin/shp2pgsql -a -W latin1 $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
+    $SHP2PGSQL -a -W latin1 $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
 done
 
 # Create tables for Vägkartan before actually inserting to them
 echo Creating structure for Vägkartan
 for F in $GSD_PATH/vagk/south/*.shp; do
     TABLE_NAME=vagk_`basename $F .shp | cut -d_ -f1`
-    /usr/lib/postgresql/9.1/bin/shp2pgsql -p -W latin1 -I $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
+    $SHP2PGSQL -p -W latin1 -I $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
 done
 
 # Actually import Vägkartan
@@ -54,5 +56,5 @@ FILES="`find $GSD_PATH/vagk/ -maxdepth 2 -name "*.shp" -not \( -name "tx_*.shp" 
 for F in $FILES; do
     TABLE_NAME=vagk_`basename $F .shp | cut -d_ -f1`
     echo Importing $F into $TABLE_NAME
-    /usr/lib/postgresql/9.1/bin/shp2pgsql -a -W latin1 $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
+    $SHP2PGSQL -a -W latin1 $F $TABLE_NAME 2>>$ERROR_LOG | tee -a $SQL_LOG | psql -d $DB 1>/dev/null 2>>$ERROR_LOG
 done
