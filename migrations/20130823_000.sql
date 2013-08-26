4af4d5d0146d91bd73d19cfcf252dd3e805624c0
perliedman <per.liedman@kartena.se>
2013-08-26 13:53:26 +0200

    Added waterways_low

---
+++ migrations/20130823_000.sql
@@ -0,0 +1,31 @@
+
+create table lmv_bright.waterway_low (
+    gid int primary key,
+    type varchar(16)
+);
+
+select AddGeometryColumn('lmv_bright', 'waterway_low', 'the_geom', 3006, 'MULTILINESTRING', 2);
+
+insert into lmv_bright.waterway_low
+	select gid,
+		case
+		when width >= 6 then 'river'
+		when width >= 4 then 'canal'
+		when width >= 2 then 'stream'
+		else 'ditch' end as type,
+		the_geom
+	from (
+		select gid, (80 - (kkod % 100)) / 10 as width, the_geom
+		from oversikt_hl
+		where kkod >= 9110
+		) as oversikt_hl_width;
+
+insert into lmv_bright.waterway_low
+	select gid,
+	'canal' as type,
+		the_geom
+	from oversikt_hl
+		where kkod in (9021, 9022);
+
+create index on lmv_bright.place (type);
+create index on lmv_bright.waterway_low using gist (the_geom);
