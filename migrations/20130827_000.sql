bfc8407b831bff3aef73ec360e97ca64379810fb
perliedman <per.liedman@dotnetmentor.se>
2015-04-23 10:14:59 +0200

    Add semicolon

--- migrations/20130827_000.sql
+++ migrations/20130827_000.sql
@@ -1,6 +1,6 @@
 alter table terrang_hl drop constraint enforce_srid_the_geom;
 
-update terrang_hl set the_geom=st_setsrid(the_geom,3006)
+update terrang_hl set the_geom=st_setsrid(the_geom,3006);
 
 ALTER TABLE terrang_hl
   ADD CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = (3006));
