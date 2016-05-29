3ba4719a9330c5af1dbc647cbfcba0c868e869ff
Peter Thorin <peter.thorin@kartena.se>
2016-05-29 15:13:15 +0200

    Added useful informative comment

---
+++ scripts/rename_geom_oversikt.sql
@@ -0,0 +1,39 @@
+/*
+   In the case that the import created columns called geom instead of the_geom,
+   use this script to fix it.
+ */
+alter table oversikt_af rename column geom to the_geom;
+alter table oversikt_ak rename column geom to the_geom;
+alter table oversikt_al rename column geom to the_geom;
+alter table oversikt_an rename column geom to the_geom;
+alter table oversikt_ao rename column geom to the_geom;
+alter table oversikt_bo rename column geom to the_geom;
+alter table oversikt_bs rename column geom to the_geom;
+alter table oversikt_gl rename column geom to the_geom;
+alter table oversikt_gs rename column geom to the_geom;
+alter table oversikt_hl rename column geom to the_geom;
+alter table oversikt_jl rename column geom to the_geom;
+alter table oversikt_js rename column geom to the_geom;
+alter table oversikt_kl rename column geom to the_geom;
+alter table oversikt_mb rename column geom to the_geom;
+alter table oversikt_mh rename column geom to the_geom;
+alter table oversikt_mk rename column geom to the_geom;
+alter table oversikt_ml rename column geom to the_geom;
+alter table oversikt_mo rename column geom to the_geom;
+alter table oversikt_ms rename column geom to the_geom;
+alter table oversikt_my rename column geom to the_geom;
+alter table oversikt_nd rename column geom to the_geom;
+alter table oversikt_nl rename column geom to the_geom;
+alter table oversikt_ns rename column geom to the_geom;
+alter table oversikt_nv rename column geom to the_geom;
+alter table oversikt_oh rename column geom to the_geom;
+alter table oversikt_ot rename column geom to the_geom;
+alter table oversikt_qf rename column geom to the_geom;
+alter table oversikt_ql rename column geom to the_geom;
+alter table oversikt_qo rename column geom to the_geom;
+alter table oversikt_qy rename column geom to the_geom;
+alter table oversikt_rutnat rename column geom to the_geom;
+alter table oversikt_tl rename column geom to the_geom;
+alter table oversikt_tx rename column geom to the_geom;
+alter table oversikt_vl rename column geom to the_geom;
+alter table oversikt_vs rename column geom to the_geom;
---
+++ scripts/rename_geom_terrang.sql
@@ -0,0 +1,43 @@
+/*
+   In the case that the import created columns called geom instead of the_geom,
+   use this script to fix it.
+ */
+alter table terrang_al rename column geom to the_geom;
+alter table terrang_as rename column geom to the_geom;
+alter table terrang_at rename column geom to the_geom;
+alter table terrang_bl rename column geom to the_geom;
+alter table terrang_bs rename column geom to the_geom;
+alter table terrang_by rename column geom to the_geom;
+alter table terrang_fl rename column geom to the_geom;
+alter table terrang_fs rename column geom to the_geom;
+alter table terrang_gs rename column geom to the_geom;
+alter table terrang_hl rename column geom to the_geom;
+alter table terrang_hs rename column geom to the_geom;
+alter table terrang_jl rename column geom to the_geom;
+alter table terrang_js rename column geom to the_geom;
+alter table terrang_kl rename column geom to the_geom;
+alter table terrang_ks rename column geom to the_geom;
+alter table terrang_ma rename column geom to the_geom;
+alter table terrang_mb rename column geom to the_geom;
+alter table terrang_ml rename column geom to the_geom;
+alter table terrang_mo rename column geom to the_geom;
+alter table terrang_ms rename column geom to the_geom;
+alter table terrang_mv rename column geom to the_geom;
+alter table terrang_mx rename column geom to the_geom;
+alter table terrang_my rename column geom to the_geom;
+alter table terrang_nl rename column geom to the_geom;
+alter table terrang_ns rename column geom to the_geom;
+alter table terrang_od rename column geom to the_geom;
+alter table terrang_oh rename column geom to the_geom;
+alter table terrang_os rename column geom to the_geom;
+alter table terrang_ot rename column geom to the_geom;
+alter table terrang_ql rename column geom to the_geom;
+alter table terrang_rutnat rename column geom to the_geom;
+alter table terrang_sl rename column geom to the_geom;
+alter table terrang_ss rename column geom to the_geom;
+alter table terrang_tl rename column geom to the_geom;
+alter table terrang_tx rename column geom to the_geom;
+alter table terrang_tx_unique rename column geom to the_geom;
+alter table terrang_vl rename column geom to the_geom;
+alter table terrang_vo rename column geom to the_geom;
+alter table terrang_vs rename column geom to the_geom;
